c5dd1d0009c97c9f62337a469b3025fea913569c
kareem nour <kimzcodes@gmail.com>
2021-08-12 18:17:00 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const [term, setTerm] = useState('');
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          srsearch: 'apple',
+          origin: '*',
+        },
+      });
+    };
+    fetchData();
+  }, [term]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <div className='my-3'>
+            <label htmlFor='exampleFormControlInput1' className='form-label'>
+              Search Input
+            </label>
+            <input
+              type='text'
+              className='form-control'
+              id='exampleFormControlInput1'
+              onChange={(e) => setTerm(e.target.value)}
+              value={term}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Title</th>
+                <th scope='col'>Desc</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>title</td>
+                <td>Desc</td>
+              </tr>
+              <tr>
+                <th scope='row'>1</th>
+                <td>title</td>
+                <td>Desc</td>
+              </tr>
+              <tr>
+                <th scope='row'>1</th>
+                <td>title</td>
+                <td>Desc</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
