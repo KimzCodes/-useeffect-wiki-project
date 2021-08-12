b74e463d9d383ba4be9775d39ee411536d857cba
kareem nour <kimzcodes@gmail.com>
2021-08-12 20:56:23 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,41 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export default function App() {
+  const [term, setTerm] = useState('javascript');
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const respond = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResult(respond.data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const fetchResult = result.map((el) => {
+    return (
+      <tr>
+        <th scope='row' key={el.pageid}>
+          1
+        </th>
+        <td>{el.title}</td>
+        <td>{el.snippet}</td>
+      </tr>
+    );
+  });
+
   return (
     <div className='container'>
       <div className='row'>
@@ -11,6 +48,8 @@ export default function App() {
               type='text'
               className='form-control'
               id='exampleFormControlInput1'
+              onChange={(e) => setTerm(e.target.value)}
+              value={term}
             />
           </div>
         </div>
@@ -26,23 +65,7 @@ export default function App() {
                 <th scope='col'>Desc</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>title</td>
-                <td>Desc</td>
-              </tr>
-              <tr>
-                <th scope='row'>1</th>
-                <td>title</td>
-                <td>Desc</td>
-              </tr>
-              <tr>
-                <th scope='row'>1</th>
-                <td>title</td>
-                <td>Desc</td>
-              </tr>
-            </tbody>
+            <tbody>{fetchResult}</tbody>
           </table>
         </div>
       </div>
