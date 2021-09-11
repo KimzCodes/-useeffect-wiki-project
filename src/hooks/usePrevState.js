7ba99b87dc70ccfea1f7fb0286d086b6a9823b5e
kareem nour <kimzcodes@gmail.com>
2021-09-12 01:26:44 +0200

    up

--- src/hooks/usePrevState.js
+++ src/hooks/usePrevState.js
@@ -1,13 +1,15 @@
 import {useEffect, useRef} from 'react'
 
 const usePrevState = (state) => {
-  const ref = useRef(); 
-
-  useEffect(() => {
-    ref.current = state
-  })
-
+ 
+   const ref = useRef(); 
+   
+    useEffect(() => {
+      ref.current = state
+    })
+ 
   return ref.current;
+
 }
 
 export default usePrevState;
